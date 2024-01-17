e014bcb35d6c4a9cd672c379b3f6da6e5e6fca77
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-17 19:31:27 +0530

    pause currently playing song

--- script.js
+++ script.js
@@ -86,3 +86,30 @@ let userData = {
   currentSong: null,
   songCurrentTime: 0,
 };
+// Play a specific song by ID
+const playSong = (id) => {
+    const song = userData?.songs.find((song) => song.id === id);
+    audio.src = song.src;
+    audio.title = song.title;
+  
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+      audio.currentTime = 0;
+    } else {
+      audio.currentTime = userData.songCurrentTime;
+    }
+    userData.currentSong = song;
+    playButton.classList.add("playing");
+  
+    highlightCurrentSong();
+    setPlayerDisplay();
+    setPlayButtonAccessibleText();
+    audio.play();
+  };
+  // Pause the currently playing song
+  const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime;
+    
+    playButton.classList.remove("playing");
+    audio.pause();
+  };
+ 
